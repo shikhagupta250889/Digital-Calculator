e83d0f2c69c93b6187aa9a83811bb4f8d7442345
Shikha Gupta <shikhagupta.250889@gmail.com>
2019-01-05 18:00:37 +0100

    bug#2 fixed, Added reset button and functionality

--- index.js
+++ index.js
@@ -64,6 +64,13 @@ var calculator = {
     buttonNode.onclick = this.equals.bind(this);
     operatorBlock.appendChild(buttonNode);
 
+    //Appending reset button in operator block
+    let resetButtonNode = document.createElement('button');
+    resetButtonNode.innerText = 'AC';
+    resetButtonNode.className = "Reset";
+    resetButtonNode.onclick = this.reset.bind(this);
+    operatorBlock.appendChild(resetButtonNode);
+
     //Create operator button elements inside operator block as children
     for (let key in this.operators) {
       let buttonNode = document.createElement('button');
@@ -94,9 +101,15 @@ var calculator = {
   },
 
   execute: function execute() {
-    var opIndex = this.operands.indexOf(this.currentOperator);
+    var opIndex = this.operands.indexOf(this.operators[this.currentOperator]);
     const a = Number(this.operands.slice(0, opIndex).join(''));
     const b = Number(this.operands.slice(opIndex + 1).join(''));
+    if (isNaN(a) || isNaN(b)) {
+      this.reset();
+      this.displayResult("Invalid Input");
+      this.inputExpression = "";
+      this.displayExpression(this.inputExpression);
+    }
     const result = this[this.currentOperator](a, b);
     this.displayResult(result.toString());
     this.result = result;
@@ -110,6 +123,15 @@ var calculator = {
     this.result = false;
   },
 
+  reset: function reset() {
+    this.operands = [];
+    this.currentOperator = "";
+    this.inputExpression = '';
+    this.result = false;
+    this.displayExpression(this.inputExpression);
+    this.displayResult("0");
+  },
+
   numInput: function numInput(x) {
     if (this.inputExpression == "0") this.inputExpression = "";
     this.inputExpression += x.toString();
@@ -121,9 +143,16 @@ var calculator = {
   operatorInput: function operatorInput(x) {
     this.inputExpression += this.operators[x];
     this.displayExpression(this.inputExpression);
-    this.currentOperator = x;
+    this.checkLastInput(x);
+    // this.currentOperator = x;
     if (this.result !== false) this.operands = [this.result.toString()];
-    this.operands.push(x);
+    this.operands.push(this.operators[x]);
+  },
+
+  checkLastInput: function checkLastInput(x) {
+    operandLength = this.operands.length;
+    if (isNaN(this.operands[operandLength-1])) return;
+    this.currentOperator = x;
   },
 
   displayExpression: function displayExpression(x) {
